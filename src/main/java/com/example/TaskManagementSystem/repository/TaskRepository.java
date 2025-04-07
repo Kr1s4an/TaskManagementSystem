032f8c34dfad69c85ca49810a152a88dcb6b9bce
kr1s4an <krisivratza@gmail.com>
2025-04-07 14:31:31 +0300

    Creating task model, repository and enum for the status and for the priority.

---
+++ src/main/java/com/example/TaskManagementSystem/repository/TaskRepository.java
@@ -0,0 +1,12 @@
+package com.example.TaskManagementSystem.repository;
+
+import com.example.TaskManagementSystem.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByAssignedUserId(Long userId);
+}
